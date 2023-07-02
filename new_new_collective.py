8a0c81a25344db049bcad21cd81e06661c4df6bb
xuerlee <xuer0324@sina.com>
2023-07-02 01:20:46 -0400

    add newest CAD dataset

---
+++ new_new_collective.py
@@ -0,0 +1,14 @@
+import numpy as np
+import skimage.io
+import skimage.transform
+
+import torch
+import torchvision.transforms as transforms
+from torch.utils import data
+import torchvision.models as models
+
+from PIL import Image
+import random
+
+import sys
+
